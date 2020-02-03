4aaabe6148e8216d894db13ee5151b7f6f15c5dd
Kim hyunwoo <snltpdua@gmail.com>
2020-02-03 21:00:08 +0900

    Updates

---
+++ precache-manifest.f4547de03579c3ef52d36ec73efcd2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03405cc882b9c34ebb11050588da00a8",
+    "url": "/cartfolio/index.html"
+  },
+  {
+    "revision": "98021ffbb310e08a1843",
+    "url": "/cartfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f7f6d4407636e2aa709",
+    "url": "/cartfolio/static/js/2.be125ad2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cartfolio/static/js/2.be125ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98021ffbb310e08a1843",
+    "url": "/cartfolio/static/js/main.49e3f77a.chunk.js"
+  },
+  {
+    "revision": "dfa92c7ee55abb17bf41",
+    "url": "/cartfolio/static/js/runtime-main.54b60aef.js"
+  }
+]);
